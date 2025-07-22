2db99f22417673514ceaa8fca570278c3c6e037b
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-07-22 13:19:43 +0000

    (refacto) plots

--- quant_research_toolbox/plots.py
+++ quant_research_toolbox/plots.py
@@ -2,11 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import Optional
 
-def bollinger_bands(price, 
-                    window:Optionel[int]=20,
+def bollinger_bands(price: pd.Series, 
+                    window:Optional[int]=20,
                     num_std:Optional[int]=2) -> None:
     """
+    Return a plot of prices with corresponding Bollinger Bands.
 
+    Args:
+        price (pd.Series): Series of prices indexed by time.
+        window (Optional, int): Window period for computing the SMA. Defaults to `20`.
+        num_std (Optional, int): Number of standard deviations for the bands. Defaults to `2`.
+
+    Returns:
+        None.
     """
 
     sma = price.rolling(
@@ -27,8 +35,8 @@ def bollinger_bands(price,
     plt.figure(figsize=(12,6))
     plt.plot(price, label="Price")
     plt.plot(sma, label = f"SMA {window} periods")
-    plt.plot(upper_bound, linestyle="--")
-    plt.plot(lower_bound, linestyle="--")
+    plt.plot(upper_bound, linestyle="--", label = "Upper Bollinger Band")
+    plt.plot(lower_bound, linestyle="--", label="Lower Bollinger Band")
     plt.fill_between(price.index,
                     lower_bound,
                     upper_bound,
