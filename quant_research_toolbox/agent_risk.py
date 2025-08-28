5aafb72d829e2caaf837b517b739a402dc5555dc
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-08-28 16:26:09 +0000

    (wip) agent

--- quant_research_toolbox/agent_risk.py
+++ quant_research_toolbox/agent_risk.py
@@ -4,13 +4,33 @@ from langgraph.prebuilt import ToolNode
 from langchain_openai import ChatOpenAI
 from typing import TypedDict, Any, Dict
 
-from utils import load_ticker_data, compute_returns, compute_cumulative_returns
+from utils import load_ticker_data
 from strategies import Momentum, MeanReversion
 from risk import ValueAtRisk, Drawdown, DownsideRisk, TailRisk
 from risk_adjusted import sharpe_ratio, sortino_ratio, omega_ratio
 
 class RiskState(TypedDict):
     risk: Dict[str, Any]
+
+def tool_risk_adjusted(state:RiskState):
+    """The risk agent computes risk adjusted metrics for the given equity."""
+
+    df_stock = load_ticker_data(
+        state["ticker_name"],
+        state["start_date"],
+        state["end_date"]
+    )
+
+    df_sma_crossover = Momentum.sma_crossover(df_stock, 
+                                              "ohlc_price")
+    
+    sr = (
+        sharpe_ratio(
+            df_sma_crossover["returns"]
+        )
+    )
+
+    return {"sharpe_ratio":sr}
 
 # Add llm here
 llm = ChatOpenAI(temperature=0)
