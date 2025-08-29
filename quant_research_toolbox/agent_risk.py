ea7d7402238c375ffe0e8adb1e428a80e4a2e21c
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-08-29 12:45:42 +0000

    (wip) add LLM parsing

--- quant_research_toolbox/agent_risk.py
+++ quant_research_toolbox/agent_risk.py
@@ -1,42 +1,68 @@
 import os
 from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode
 from langchain_openai import ChatOpenAI
-from typing import TypedDict, Any, Dict
+from typing import TypedDict, Any, Dict, Literal
+import json
 
 from utils import load_ticker_data
 from strategies import Momentum, MeanReversion
 from risk import ValueAtRisk, Drawdown, DownsideRisk, TailRisk
 from risk_adjusted import sharpe_ratio, sortino_ratio, omega_ratio
 
-class RiskState(TypedDict):
-    risk: Dict[str, Any]
 
-def tool_risk_adjusted(state:RiskState):
-    """The risk agent computes risk adjusted metrics for the given equity."""
+# ------------- State -------------  
+class StrategyState(TypedDict):
+    query: str
+    ticker: str
+    start_date: str
+    end_date: str
+    strategy: Literal["sma_crossover", "naive_momentum", "sma_mean_reversion"]
+    returns: Any
+    performance: Dict[str, float]
 
-    df_stock = load_ticker_data(
-        state["ticker_name"],
-        state["start_date"],
-        state["end_date"]
-    )
+# ------------- Nodes -------------
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-    df_sma_crossover = Momentum.sma_crossover(df_stock, 
-                                              "ohlc_price")
-    
-    sr = (
-        sharpe_ratio(
-            df_sma_crossover["returns"]
-        )
-    )
+def parse_query(state:StrategyState) -> StrategyState:
+    """
+    Use the LLM to parse the query and identify the ticker of interest,
+    the start and end dates for the strategy backtest, the type of 
+    strategy to apply on the ticker.
+    """
 
-    return {"sharpe_ratio":sr}
+    prompt = f"""
+    Extract the following field from the user query:
+    - ticker (stock symbol, e.g. AAPL)
+    - start_date (YYYY-MM-DD)
+    - end_date (YYYY-MM-DD)
+    - strategy ()
+    """
 
-# Add llm here
-llm = ChatOpenAI(temperature=0)
+    structured = llm.invoke(prompt).content
+    parsed = json.loads(structured)
 
-risk_graph = StateGraph(RiskState)
-risk_graph.add_node("tool_risk_adjusted", tool_risk_adjusted)
-risk_graph.add_edge(START, "tool_risk_adjusted")
-risk_graph.add_edge("tool_risk_adjusted", END)
-risk_compiled_graph = risk_graph.compile()
+    return {
+        "ticker":parsed["ticker"],
+        "start_date":parsed["start_date"],
+        "end_date":parsed["end_date"],
+        "strategy":parsed["strategy"]
+    }
+
+# ------------- Graph -------------   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
